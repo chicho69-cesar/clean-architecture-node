046c5d0f9a1958f16ece07730424feb67c4f32ec
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-06-11 18:35:52 -0600

    :sparkles: Update envs adapter

--- src/config/adapters/envs.adapter.ts
+++ src/config/adapters/envs.adapter.ts
@@ -2,10 +2,20 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const envs = {
-  PORT: get('PORT').required().asPortNumber(),
+  PORT: get('PORT')
+    .required()
+    .asPortNumber(),
 
-  MONGO_URL: get('MONGO_URL').required().asString(),
-  MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+  DB_TYPE: get('DB_TYPE')
+    .default('mongo')
+    .asEnum(['mongo', 'postgres', 'mysql']),
+
+  MONGO_URL: get('MONGO_URL').asString(),
+  MONGO_DB_NAME: get('MONGO_DB_NAME').asString(),
+
+  POSTGRES_URL: get('POSTGRES_URL').asString(),
+
+  MYSQL_URL: get('MYSQL_URL').asString(),
 
   JWT_SEED: get('JWT_SEED').required().asString(),
 };
