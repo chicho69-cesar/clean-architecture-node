39d3659480667bfea353e4feb245cbf524eaf1c6
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-06-24 00:10:47 -0600

    :sparkles: Add auth datasource domain

---
+++ src/domain/datasources/auth.datasource.ts
@@ -0,0 +1,10 @@
+import type { LoginUserDto } from '../dtos/auth/login-user.dto';
+import type { RegisterUserDto } from '../dtos/auth/register-user.dto';
+import type { UserEntity } from '../entities/user.entity';
+
+export abstract class AuthDataSource {
+  public abstract login(loginUserDto: LoginUserDto): Promise<UserEntity>;
+  public abstract logout(userId: string): Promise<void>;
+  public abstract refreshToken(userId: string): Promise<string>;
+  public abstract register(registerUserDto: RegisterUserDto): Promise<UserEntity>;
+}
