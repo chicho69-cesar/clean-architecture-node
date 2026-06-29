2168173699c2acb326b2ac3853f840107c4df632
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-06-29 08:13:30 -0600

    :sparkles: Add server implementation

---
+++ src/presentation/server.ts
@@ -0,0 +1,33 @@
+import express, { Router } from 'express';
+
+interface Options{
+  port?: number;
+  routes: Router;
+}
+
+export class Server {
+  private readonly app = express();
+  private readonly port: number;
+  private readonly routes: Router;
+
+  public constructor(options: Options) {
+    const { port = 3000, routes } = options;
+
+    this.port = port;
+    this.routes = routes;
+  }
+
+  public async start() {
+    // Middlewares
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
+    // Usar las rutas definidas
+    this.app.use(this.routes);
+
+    // Escuchar el puerto
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
